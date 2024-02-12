484d547c586dccbe3621bae17dd87970017d218a
Jasmeet Singh <jasmeetsinghworking@gmail.com>
2024-02-12 21:08:21 +0530

    Final Commit

---
+++ index.js
@@ -0,0 +1,12 @@
+var navLinks = document.getElementById("navLinks");
+
+function showMenu() {
+    navLinks.style.right = "0";
+}
+
+function hideMenu() {
+    navLinks.style.right = "-200px";
+}
+
+const year = new Date().getFullYear();
+document.getElementById("year").innerHTML = year;
